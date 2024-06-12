69a8afde0c87dccfc41ca783fc3dc2c8cc477202
imbianyunren <60705979+imbianyunren@users.noreply.github.com>
2024-06-13 03:47:18 +0800

    AtherisFuzzTesting

---
+++ Stockbot/fuzz_test/test_get_month_revenue_fuzz.py
@@ -0,0 +1,32 @@
+import unittest
+import datetime as dt
+import pandas as pd
+import atheris
+import sys
+from stockapi import StockAPI
+
+class TestStockAPIFuzz(unittest.TestCase):
+
+    atheris.instrument_all()
+
+    def setUp(self):
+        self.stockapi = StockAPI()
+        
+
+    def test_get_month_revenue_fuzz(self):
+        def test_one_input(input_bytes):
+            fdp = atheris.FuzzedDataProvider(input_bytes)
+            # stock_number = fdp.ConsumeUnicodeNoSurrogates(10)
+            stock_number = str(fdp.ConsumeIntInRange(0, 9999)).zfill(4)
+            year = fdp.ConsumeIntInRange(2010, 2050)
+            month = fdp.ConsumeIntInRange(1, 12)
+            try:
+                self.stockapi.get_month_revenue(stock_number, year, month)
+            except (ValueError, TypeError):
+                pass
+
+        atheris.Setup(sys.argv, test_one_input)
+        atheris.Fuzz()
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Stockbot/fuzz_test/test_realtime_fuzz.py
@@ -0,0 +1,30 @@
+import unittest
+import datetime as dt
+import pandas as pd
+import atheris
+import sys
+from stockapi import StockAPI
+
+class TestStockAPIFuzz(unittest.TestCase):
+
+    atheris.instrument_all()
+
+    def setUp(self):
+        self.stockapi = StockAPI()
+
+    def test_realtime_fuzz(self):
+        def test_one_input(input_bytes):
+            fdp = atheris.FuzzedDataProvider(input_bytes)
+            # stock_number = fdp.ConsumeUnicodeNoSurrogates(10)
+            stock_number = str(fdp.ConsumeIntInRange(0, 9999)).zfill(4)
+            
+            try:
+                self.stockapi.realtime(stock_number)
+            except (ValueError, TypeError, KeyError):
+                pass
+
+        atheris.Setup(sys.argv, test_one_input)
+        atheris.Fuzz()
+
+if __name__ == "__main__":
+    unittest.main()
